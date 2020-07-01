b7a4a1dd94a8184601b4be00db6709027b0b607b
Rachel de la Torre <Rachel.delatorre1@gmail.com>
2020-07-01 22:43:23 +0200

    iteration 3

--- app.js
+++ app.js
@@ -24,11 +24,9 @@ mongoose
     console.error("Error connecting to mongo", err);
   });
 
-
-
-
 const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`
+);
 
 const app = express();
 
@@ -45,51 +43,36 @@ app.use(cookieParser());
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 
-
-// Express View engine setup
-//-------------------------------------------------------------------somethinggggg????
-
+//-------------------------------------------------------------------timeouts
 // app.use(
-//     require("node-sass")({
-//       src: path.join(__dirname, "public"),
-//       dest: path.join(__dirname, "public"),
-//       sourceMap: true
+//     session({
+//       secret: process.env.SESSION_SECRET,
+//       cookie: { maxAge: 24 * 60 * 60 * 1000 },
+//       // session is uninitialized when it is new but not modified - default is false
+//       saveUninitialized: false,
+//       //Forces the session to be saved back to the session store, 
+//       // even if the session was never modified during the request.
+//       resave: true,
+//       store: new MongoStore({
+//         //When the session cookie has an expiration date, connect-mongo will use it.
+//         // Otherwise, it will create a new one, using ttl option - here ttl is one day.
+//         mongooseConnection: mongoose.connection,
+//         ttl: 24 * 60 * 60 * 1000
+//       })
 //     })
-//   );
+// );
+// Express View engine setup
+
+
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-// default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
-
-
-
-//-------------------------------------------------------------------timeouts
-// app.use(
-    session({
-      secret: process.env.SESSION_SECRET,
-      cookie: { maxAge: 24 * 60 * 60 * 1000 },
-      // session is uninitialized when it is new but not modified - default is false
-      saveUninitialized: false,
-      //Forces the session to be saved back to the session store, 
-      // even if the session was never modified during the request.
-      resave: true,
-      store: new MongoStore({
-        //When the session cookie has an expiration date, connect-mongo will use it.
-        // Otherwise, it will create a new one, using ttl option - here ttl is one day.
-        mongooseConnection: mongoose.connection,
-        ttl: 24 * 60 * 60 * 1000
-      })
-    })
-
-
-
-
-
-
+// default value for title localapp.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = "Express - Generated with IronGenerator";
 
 
 
@@ -100,11 +83,6 @@ app.use('/', index);
 
 const auth = require('./routes/auth');
 app.use('/', auth);
-
-
-
-
-
 
 
 module.exports = app;
